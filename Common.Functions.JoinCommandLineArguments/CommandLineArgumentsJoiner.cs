cd072a8d3f3888f9e3e7fca3b0cce525cb309d57
Christopher Dorst <chris@dorst.email>
2017-12-27 12:44:05 -0600

    Adding .Join() helper

---
+++ Common.Functions.JoinCommandLineArguments/CommandLineArgumentsJoiner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using static Common.Functions.JoinStringsWithSpace.StringsJoiner;
+using static Common.Functions.PrependEnumerationElement.EnumerationElementPrepender;
+
+namespace Common.Functions.JoinCommandLineArguments
+{
+    public static class CommandLineArgumentsJoiner
+    {
+        public static string Join(string firstArgument, IEnumerable<string> remainingArguments)
+            => JoinWithSpace(
+                Prepend(firstArgument, remainingArguments));
+    }
+}
